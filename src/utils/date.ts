0ed5c70d0fe37d8582e107a3ae76056b9ff4a06c
felipejoq <felipejofreq@gmail.com>
2024-10-19 23:00:28 -0300

    cambia forma de obtener fechas

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,9 +1,12 @@
+import { format } from "@formkit/tempo"
+
 export const CHILE_CONTINENTAL = 'America/Santiago';
 export const CHILE_AUSTRAL = 'America/Punta_Arenas';
 export const CHILE_PENINSULAR = 'Pacific/Easter';
 
 const getCurrentYearInTimeZone = (timeZone: string): number => {
-    return Number(new Date().toLocaleString('es-CL', { timeZone, year: 'numeric' }));
+
+    return Number(format(new Date(), "YYYY", "cl"));
 };
 
 export const currentYear = getCurrentYearInTimeZone(CHILE_CONTINENTAL);
