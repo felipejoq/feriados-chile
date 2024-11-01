033aeb8b1bf84bc7f32eef4aded9b69830425a15
felipejoq <felipejofreq@gmail.com>
2024-11-01 10:09:22 -0300

    Algunos cambios pequeños que impactan el SEO

---
+++ src/utils/config.global.ts
@@ -0,0 +1,7 @@
+const year = new Date().getFullYear();
+import SocialCover from '@media/social-cover.jpg';
+
+export const SITE_NAME = 'Feriados.app';
+export const SITE_DESCRIPTION = `Lista completa de los feriados en Chile para el año ${year}`;
+export const SITE_IMAGE = SocialCover;
+export const SITE_AUTHOR = 'Feriados.app';
